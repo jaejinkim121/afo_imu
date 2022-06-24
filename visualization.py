7960f6273c97b9a7ae7868cecee1dbe98c759de7
jaejinkim121 <gisgen9@snu.ac.kr>
2022-06-24 16:57:58 +0900

    add basic skeleton

---
+++ visualization.py
@@ -0,0 +1,17 @@
+import matplotlib
+import numpy as np
+from scipy.spatial.transform import Rotation as R
+
+
+def make_data_structure(data):
+    return
+
+
+def main():
+
+
+    return
+
+
+if __name__ == "__main__":
+    main()
